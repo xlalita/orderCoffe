796925966daa7b628a51cb9987ea780be0dc4067
xlalita <54805590+xlalita@users.noreply.github.com>
2019-09-30 19:02:41 +0700

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.coffe.ordercoffe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void submitOrder(View view){
+        display(1);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+         quantityTextView.setText("" + number);
+
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
